fc709c1552e903a9bd44514fb1727eba1ba85190
Gustavo Burns Soares <gburnssoares@gmail.com>
2025-04-22 14:55:24 -0300

    Geração de Clases Department e Seller

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(1, "Gustavo", "TESTE@GMAIL>COM", new Date(), 3000.00 , obj);
+		System.out.println(seller);
+	}
+
+}
