40f86233dd630ae7a784eb0e22789de578dfb04c
Gustavo Burns Soares <gburnssoares@gmail.com>
2025-10-20 20:08:11 -0300

    WIP - add findByDepartment

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,72 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDAO;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class DepartmentDaoJDBC implements DepartmentDAO {
+
+	private Connection conn;
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			String query = "SELECT * " + "FROM department " + "WHERE id = ?";
+			st = conn.prepareStatement(query);
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				return dep;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -61,6 +64,38 @@ public class SellerDaoJDBC implements SellerDAO {
 			DB.closeResultSet(rs);
 		}
 
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department departament) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		List<Seller> sellerList = new ArrayList<>();
+		try {
+			String query = "SELECT seller.*, department.Name DepName " + "FROM seller "
+					+ " INNER JOIN department ON seller.DepartmentId = department.Id " + "WHERE department.Id = ? "
+					+ "ORDER BY Name";
+			st = conn.prepareStatement(query);
+			st.setInt(1, departament.getId());
+			rs = st.executeQuery();
+			Map<Integer, Department> map = new HashMap<>();
+			while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller obj = instatiateSeller(rs, dep);
+				sellerList.add(obj);
+				return sellerList;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	private Seller instatiateSeller(ResultSet rs, Department dep) throws SQLException {
